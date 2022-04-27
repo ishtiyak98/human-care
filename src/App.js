00693dc4896b02f9450a97acbd46e666058323bc
ishtiyak98 <rifatishtiyak@gmail.com>
2022-04-27 13:11:36 +0600

    fetch and axios added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from './components/Header/Header';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage/HomePage';
+import LogIn from './components/LogIn/LogIn';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path='/home' element={<HomePage></HomePage>}></Route>
         <Route path='/' element={<HomePage></HomePage>}></Route>
+        <Route path='/register' element={<LogIn></LogIn>}></Route>
       </Routes>
     </div>
   );
--- src/components/CharityEvents/CharityEvents.js
+++ src/components/CharityEvents/CharityEvents.js
@@ -5,7 +5,7 @@ const CharityEvents = () => {
     const [events, setEvents] = useState([]);
 
    useEffect(()=>{
-    fetch("events.json")
+    fetch("http://localhost:5000/events")
     .then(res=>res.json())
     .then(data => setEvents(data));
    },[]);
@@ -15,7 +15,7 @@ const CharityEvents = () => {
         <div className='px-0 lg:px-24 lg:pb-[100px]'>
             <div className='grid grid-cols-1 lg:grid-cols-4 gap-y-20 gap-x-20'>
                 {
-                    events.map(event=><EventCard event={event}></EventCard>)
+                    events.map(event=><EventCard key={event._id} event={event}></EventCard>)
                 }
             </div>
         </div>
